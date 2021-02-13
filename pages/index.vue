d5c15a5076c9c0ebe737226e5736ea4e684ff342
taras-kryvko <taras.kryvko@hypedriven.com>
2021-02-13 19:20:37 +0200

    redirect to page1

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
-  <v-row justify="center" align="center">
- fsd
+  <v-row justify="center" align="center"> 
+    
   </v-row>
 </template>
 
 <script>
  
 export default {
-  components: {}
+  components: {},
+  middleware: 'default'
 }
 </script>
